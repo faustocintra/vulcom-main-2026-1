e60c8a9c44b8bbcbeb308735c917947ed22abe7f
hugo14253600 <hugohenrique071325@hotmail.com>
2026-05-09 10:50:29 -0300

    (09/05) Armazenamento do token JWT em cookie HTTP-Only e seguro

--- back-end/src/controllers/users.js
+++ back-end/src/controllers/users.js
@@ -154,6 +154,10 @@ controller.login = async function(req, res) {
      // Se a senha estiver errada, retorna
      // HTTP 401: Unauthorized
      if(! passwordIsValid) return res.status(401).end()
+      
+    // Eliminamos o campo "password" dos dados do usuário antes de incluí-lo
+     // no payload do token JWT
+     if(user.password) delete user.password
 
       if(! passwordIsValid) return res.status(401).end()
 
@@ -175,7 +179,8 @@ controller.login = async function(req, res) {
 
       // Retorna o token e o usuário autenticado com
       // HTTP 200: OK (implícito)
-      res.send({token, user})
+       res.send({user})
+
 
   }
   catch(error) {
@@ -190,6 +195,17 @@ controller.me = function(req, res) {
   // Retorna as informações do usuário autenticado
   // HTTP 200: OK (implícito)
   res.send(req?.authUser)
+}
+
+controller.logout = function(req, res) {
+ // Apaga no front-end o cookie que armazena o token de autorização
+ res.clearCookie(process.env.AUTH_COOKIE_NAME, {
+   path: '/',
+   secure: true,
+   sameSite: 'None'
+ })
+ // HTTP 204: No Content
+ res.status(204).end()
 }
 
 export default controller
